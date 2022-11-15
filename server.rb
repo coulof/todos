0234a22cf35b512687e098127ce7334ddc6cb22e
coulof <florian.coulombel@dell.com>
2022-11-15 15:37:12 -0500

    WIP with external redis

--- server.rb
+++ server.rb
@@ -12,9 +12,9 @@ set :dump_errors, true
 redis_host = ENV['REDIS_HOST']
 redis_host ||= '127.0.0.1'
 
-#redis_port = ENV['REDIS_PORT'].to_i
-#redis_port ||= 6379
-
+redis_port = ENV['REDIS_PORT'].to_i
+redis_port ||= 6379
+puts "connect to #{redis_host}:#{redis_port}"
 #redis = Redis.new(host: redis_host, port: redis_port)
 redis = Redis.new(host: redis_host)
 #redis.set('todos', '{}')
